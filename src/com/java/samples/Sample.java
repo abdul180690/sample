09dbe9745507017accfd37f0cd2ab7fbea3ac8a6
Abdullah <abdul@gmail.com>
2024-10-05 11:46:17 -0500

    sample commit

---
+++ src/com/java/samples/Sample.java
@@ -0,0 +1,9 @@
+package com.java.samples;
+import java.lang.System;
+public class Sample {//class definition/body
+    public static void main(String[] args) {//method definition/body
+        System.out.println("welcome to java");//logic section
+    }
+}
+//inputfsdfdsf
+//output
