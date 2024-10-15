37274204a08c0dc413026bb1312a6a12e5ece209
abdul180690 <abdullah180690.carrier@gmail.com>
2024-10-15 17:51:29 +0530

    * main function updated with sub functions * array_addition() function updated

--- src/com/java/samples/Arrays_samples.java
+++ src/com/java/samples/Arrays_samples.java
@@ -5,9 +5,15 @@ import java.util.Scanner;
 
 public class Arrays_samples {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+
+    //    next5_OddorEven();
+        array_Addition();
+    }
+
+    static void next5_OddorEven() {
         int[] arr= new int[5];
         System.out.println("Enter the number: ");
+        Scanner sc = new Scanner(System.in);
         int input=sc.nextInt()+2;
         for(int i=0;i<arr.length;i++){
             arr[i]=input;
@@ -20,5 +26,27 @@ public class Arrays_samples {
         for (int a : arr) {                                     // Enhanced for loop
             System.out.print(a+" ");
         }
+    }
+    static void array_Addition(){
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the Size of the Arrays: ");
+        int size=in.nextInt();
+        int[] a1=new int[size];
+        int[] a2=new int[size];
+        int[] result=new int[size];
+        System.out.println("Enter Value for the First Array: ");
+        for(int i=0;i<size;i++) {
+            System.out.print("Value at index " + i + ": ");
+            a1[i] = in.nextInt();
+        }
+        System.out.println("Enter the second array values: ");
+        for(int i=0;i<size;i++) {
+            System.out.print("Value at index " + i + ": ");
+            a2[i] = in.nextInt();
+        }
+        for(int i=0;i<size;i++) {
+            result[i]=a1[i]+a2[i];
+        }
+        System.out.print("Result : " + Arrays.toString(result) );
     }
 }
